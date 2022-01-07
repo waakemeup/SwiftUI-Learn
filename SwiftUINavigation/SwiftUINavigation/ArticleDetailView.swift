794e75fe6baa5facafa421ec6738a28725133aa4
Ruby <LIU10032@163.com>
2022-01-07 13:14:14 +0800

    feat: navigation finish

--- SwiftUINavigation/SwiftUINavigation/ArticleDetailView.swift
+++ SwiftUINavigation/SwiftUINavigation/ArticleDetailView.swift
@@ -19,9 +19,37 @@ struct ArticleDetailView: View {
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                 
+                Group {
+                    Text(article.title)
+                        .font(.system(.title, design: .rounded))
+                        .fontWeight(.black)
+                        .lineLimit(3)
+                    
+                    Text("By \(article.author)".uppercased())
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
+                .padding(.bottom,0)
+                .padding(.horizontal)
                 
+                Text(article.content)
+                    .font(.body)
+                    .padding()
+                    .lineLimit(1000)
+                    .multilineTextAlignment(.leading)
             }
         }
+        .edgesIgnoringSafeArea(.top)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading:
+            Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }, label: {
+                Image(systemName: "chevron.left.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+            })
+        )
     }
 }
 
--- SwiftUINavigation/SwiftUINavigation/ContentView.swift
+++ SwiftUINavigation/SwiftUINavigation/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    init(){
+        let color = UIView()
+        color.backgroundColor = UIColor.red
+        UITableViewCell.appearance().selectedBackgroundView = color
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(articles){ article in
+                ZStack {
+                    ArticleRow(article: article)
+                    
+                    NavigationLink(destination: ArticleDetailView(article: article)){
+                        EmptyView()
+                    }
+                }
+            }
+            .navigationBarTitle("Your Reading")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ArticleRow:View {
+    var article:Article
+    
+    var body: some View {
+        VStack(alignment:.leading){
+            Image(article.image)
+                .resizable()
+                .aspectRatio(contentMode:.fit)
+                .cornerRadius(5)
+            
+            Text(article.title)
+                .font(.system(.title,design: .rounded))
+                .fontWeight(.black)
+                .lineLimit(3)
+                .padding(.bottom,0)
+            
+            Text("By \(article.author)".uppercased())
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                .padding(.bottom,0)
+            
+            HStack(spacing:3){
+                ForEach(1...(article.rating),id:\.self){ _ in
+                    Image(systemName: "star.fill")
+                        .font(.caption)
+                        .foregroundColor(.yellow)
+                }
+            }
+            
+            Text(article.excerpt)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+        }
     }
 }
