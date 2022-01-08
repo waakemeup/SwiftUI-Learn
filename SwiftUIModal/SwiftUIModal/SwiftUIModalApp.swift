f3ad5a0fcac6dd5e3c3c26180e3af53961264e2c
Ruby <LIU10032@163.com>
2022-01-08 21:49:06 +0800

    feat: swiftUIModal init

---
+++ SwiftUIModal/SwiftUIModal/SwiftUIModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIModalApp.swift
+//  SwiftUIModal
+//
+//  Created by Ruby on 2022/1/8.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
