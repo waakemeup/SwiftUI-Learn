4134888c9af5ae48258d4719425ec3ccf4ccd9ce
Ruby <LIU10032@163.com>
2022-01-09 21:22:47 +0800

    feat: swiftuiform init

---
+++ SwiftUIForm/SwiftUIForm/SwiftUIFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFormApp.swift
+//  SwiftUIForm
+//
+//  Created by Ruby on 2022/1/9.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
