d788a3d37814af6d98f8d1c91c64a613ed3cf02d
Ruby <LIU10032@163.com>
2022-01-11 21:01:59 +0800

    feat: the form UI

---
+++ SwiftUIFormRegistrationUI/SwiftUIFormRegistrationUI/SwiftUIFormRegistrationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFormRegistrationUIApp.swift
+//  SwiftUIFormRegistrationUI
+//
+//  Created by Ruby on 2022/1/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFormRegistrationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
