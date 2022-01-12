4da5c246af7f499d1c8fe9de02cf3ee822b420e3
Ruby <LIU10032@163.com>
2022-01-12 23:56:13 +0800

    feat: initial file

--- SwiftUIActionSheet/SwiftUIActionSheet/ContentView.swift
+++ SwiftUIActionSheet/SwiftUIActionSheet/ContentView.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var restaurants = [ Restaurant(name: "Cafe Deadend", image: "cafedeadend"),
+                   Restaurant(name: "Homei", image: "homei"),
+                   Restaurant(name: "Teakha", image: "teakha"),
+                   Restaurant(name: "Cafe Loisl", image: "cafeloisl"),
+                   Restaurant(name: "Petite Oyster", image: "petiteoyster"),
+                   Restaurant(name: "For Kee Restaurant", image: "forkeerestaurant"),
+                   Restaurant(name: "Po's Atelier", image: "posatelier"),
+                   Restaurant(name: "Bourke Street Bakery", image: "bourkestreetbakery"),
+                   Restaurant(name: "Haigh's Chocolate", image: "haighschocolate"),
+                   Restaurant(name: "Palomino Espresso", image: "palominoespresso"),
+                   Restaurant(name: "Homei", image: "upstate"),
+                   Restaurant(name: "Traif", image: "traif"),
+                   Restaurant(name: "Graham Avenue Meats And Deli", image: "grahamavenuemeats"),
+                   Restaurant(name: "Waffle & Wolf", image: "wafflewolf"),
+                   Restaurant(name: "Five Leaves", image: "fiveleaves"),
+                   Restaurant(name: "Cafe Lore", image: "cafelore"),
+                   Restaurant(name: "Confessional", image: "confessional"),
+                   Restaurant(name: "Barrafina", image: "barrafina"),
+                   Restaurant(name: "Donostia", image: "donostia"),
+                   Restaurant(name: "Royal Oak", image: "royaloak"),
+                   Restaurant(name: "CASK Pub and Kitchen", image: "caskpubkitchen")
+    ]
+    
+    @State private var selectedRestaurant: Restaurant?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            ForEach(restaurants) { restaurant in
+                BasicImageRow(restaurant: restaurant)
+            }
+
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Restaurant: Identifiable {
+    var id = UUID()
+    var name: String
+    var image: String
+    var isFavorite: Bool = false
+    var isCheckIn: Bool = false
+}
+
+struct BasicImageRow: View {
+    var restaurant: Restaurant
+    
+    var body: some View {
+        HStack {
+            Image(restaurant.image)
+                .resizable()
+                .frame(width: 40, height: 40)
+                .cornerRadius(5)
+            Text(restaurant.name)
+            
+        }
     }
 }
