9b0b480e0e7f7f21014df45c7244c855e54e061c
Ruby <LIU10032@163.com>
2022-01-12 23:48:49 +0800

    feat: init file

---
+++ SwiftUIActionSheet/SwiftUIActionSheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIActionSheet
+//
+//  Created by Ruby on 2022/1/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIActionSheet/SwiftUIActionSheet/SwiftUIActionSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIActionSheetApp.swift
+//  SwiftUIActionSheet
+//
+//  Created by Ruby on 2022/1/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIActionSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
