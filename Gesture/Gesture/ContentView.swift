5b9efafe87589995652a174252477d1632c56bc1
Ruby <LIU10032@163.com>
2022-01-16 00:17:28 +0800

    feat: learn gesture

--- Gesture/Gesture/ContentView.swift
+++ Gesture/Gesture/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var isPressed = false
     @GestureState private var longPressTap = false
+    @GestureState private var dragOffSet = CGSize.zero
+    @State private var position = CGSize.zero
     
     var body: some View {
         VStack {
@@ -37,22 +39,38 @@ struct ContentView: View {
 //                    })
 //                )
             
+//            Image(systemName: "star.circle.fill")
+//                .font(.system(size: 200))
+//                .opacity(longPressTap ? 0.4 : 1.0)
+//                .scaleEffect(isPressed ? 0.5 : 1.0)
+//                .animation(.easeInOut)
+//                .foregroundColor(.green)
+//                .gesture(
+//                    LongPressGesture(minimumDuration:1.0)
+//                        .updating($longPressTap,body:{(currentState,state,transaction) in state = currentState})
+//                        .onEnded({_ in
+//                            self.isPressed.toggle()
+//                        })
+//                )
+            
+            
+            
             Image(systemName: "star.circle.fill")
-                .font(.system(size: 200))
-                .opacity(longPressTap ? 0.4 : 1.0)
-                .scaleEffect(isPressed ? 0.5 : 1.0)
+                .font(.system(size: 100))
+                .offset(x: position.width + dragOffSet.width, y:  dragOffSet.height + position.height)
                 .animation(.easeInOut)
                 .foregroundColor(.green)
                 .gesture(
-                    LongPressGesture(minimumDuration:1.0)
-                        .updating($longPressTap,body:{(currentState,state,transaction) in state = currentState})
-                        .onEnded({_ in
-                            self.isPressed.toggle()
+                    DragGesture()
+                        .updating($dragOffSet,body:{(value,state,transaction) in
+                            state = value.translation
+                        })
+                        .onEnded({(value) in
+                            self.position.height += value.translation.height
+                            self.position.width += value.translation.width
                         })
                 )
         }
-        
-        
             
     }
 }
