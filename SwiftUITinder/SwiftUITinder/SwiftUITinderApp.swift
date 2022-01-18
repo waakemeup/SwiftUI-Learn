026aecf46b1a74fe3b99ea2fcb5ed69bc0baacc6
Ruby <LIU10032@163.com>
2022-01-18 22:22:47 +0800

    feat: tinder init

---
+++ SwiftUITinder/SwiftUITinder/SwiftUITinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITinderApp.swift
+//  SwiftUITinder
+//
+//  Created by Ruby on 2022/1/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
