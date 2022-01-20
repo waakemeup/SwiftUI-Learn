ebd99f6170da2a43c99b61d7c39399dd9882b1e8
Ruby <LIU10032@163.com>
2022-01-20 22:25:50 +0800

    feat: wallet init

---
+++ SwiftUIWallet/SwiftUIWallet/SwiftUIWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWalletApp.swift
+//  SwiftUIWallet
+//
+//  Created by Ruby on 2022/1/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
